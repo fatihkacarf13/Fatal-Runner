5a2a6992230798ee4fee10958718615478168f21
fatihkacarf13 <fatihkacar@f13entertainment.com>
2022-01-17 15:05:42 +0300

    Feat: Added boss hitted and death

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -6,8 +6,32 @@ public class Boss : MonoBehaviour
 {
     public int bossHealt = 40;
 
+    [SerializeField] private BossAnimations _bossanimStateController;
+
     public void Update()
     {
         Debug.Log(bossHealt);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PlayerPunch"))
+        {
+            if (bossHealt>10)
+            {
+                _bossanimStateController.BossHitted();
+
+            }
+            if (bossHealt<=10)
+            {
+                _bossanimStateController.BossDeath();
+            }
+           
+        }
+        
+    }
+
+    
+
+
 }
---
+++ Assets/Scripts/BossAnimations.cs
@@ -0,0 +1,37 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAnimations : MonoBehaviour
+{
+    [SerializeField] private Animator _bossAnimator;
+
+
+    [Button]
+    public void BossPunch()
+    {
+        _bossAnimator.SetTrigger("isBossPunch");
+    }
+
+    [Button]
+    public void BossHitted()
+    {
+        _bossAnimator.SetTrigger("isBossHitted");
+    }
+
+    [Button]
+    public void BossIdle()
+    {
+        _bossAnimator.SetTrigger("isBossIdle");
+    }
+
+    [Button]
+    public void BossDeath()
+    {
+        _bossAnimator.SetTrigger("isBossDeath");
+    }
+
+
+
+}
